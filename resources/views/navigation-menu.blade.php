0c266744625f165f5b2878eeb4be69ebdf8b03d4
Mohammed Ali Izgua <medali.izgua@outlook.com>
2022-06-11 18:54:35 +0100

    uncommented dashboard link

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,9 +12,9 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    {{-- <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
-                    </x-jet-nav-link> --}}
+                    </x-jet-nav-link>
                     <x-jet-nav-link href="{{ route('servers.index') }}" :active="request()->routeIs('servers.*')">
                         {{ __('Servers') }}
                     </x-jet-nav-link>
